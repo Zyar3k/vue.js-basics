2d6186b9fb3058c5e33556a8f80fb64fed07b680
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-09 14:54:59 +0200

    Render condition

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      showContent: true,
       title: "the Godfather",
       author: "Mario Puzo",
       age: 18,
@@ -10,6 +11,9 @@ const app = Vue.createApp({
     changeTitle(title) {
       // this.title = "Buh bub blur";
       this.title = "Mandana";
+    },
+    toggleShowContent() {
+      this.showContent = !this.showContent;
     },
   },
 });
